936174289095c5e4223a15c2a1ea57a36ac113f0
SRAVANNAIDU1 <155452312+SRAVANNAIDU1@users.noreply.github.com>
2024-01-05 08:53:39 +0530

    #include<stdio.h> int main() {     int  n,nn,rev=0;     scanf("%d",&n);     nn=n;     while(n>0){         rev=rev*10+n%10;         n/=10;     }     if(rev==nn){         printf("Palindrome ");     }else{         printf("Not Palindrome");     } }

---
+++ Palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int  n,nn,rev=0;
+    scanf("%d",&n);
+    nn=n;
+    while(n>0){
+        rev=rev*10+n%10;
+        n/=10;
+    }
+    if(rev==nn){
+        printf("Palindrome
+");
+    }else{
+        printf("Not Palindrome");
+    }
+}
